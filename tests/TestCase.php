14578a4311d7c088e4567ab31019ac0a246cc55f
USAMI Kenta <tadsan@zonu.me>
2020-01-12 14:39:27 +0900

    composer require nyholm/psr7 and add TestCase::createResponseFactory()

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,13 @@
 
 namespace Bag2\Cookie;
 
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseFactoryInterface;
+
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    public function createResponseFactory(): ResponseFactoryInterface
+    {
+        return new Psr17Factory;
+    }
 }
