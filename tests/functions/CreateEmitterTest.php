1aa6c40ccfa3355bd548ab95a5d20d1843b2ab0a
USAMI Kenta <tadsan@zonu.me>
2020-01-13 07:17:02 +0900

    Add Bag2\Cookie\functions\CreateEmitterTest class

---
+++ tests/functions/CreateEmitterTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bag2\Cookie\functions;
+
+use Badoo\SoftMocks;
+use Bag2\Cookie\Bag;
+use Bag2\Cookie\Emitter\Php73Function;
+use Bag2\Cookie\Emitter\PhpLegacyFunction;
+
+final class CreateEmitterTest extends \Bag2\Cookie\TestCase
+{
+    /**
+     * @dataProvider versionsProvider
+     * @param class-string $expected
+     */
+    public function test(int $php_version_id, string $expected)
+    {
+        SoftMocks::redefineConstant('PHP_VERSION_ID', $php_version_id);
+
+        $this->assertInstanceOf($expected, \Bag2\Cookie\create_emitter());
+    }
+
+    public function versionsProvider()
+    {
+        return [
+            '7.1.0' => [70100, PhpLegacyFunction::class],
+            '7.2.0' => [70200, PhpLegacyFunction::class],
+            '7.3.0' => [70300, Php73Function::class],
+            '7.4.0' => [70400, Php73Function::class],
+        ];
+    }
+}
